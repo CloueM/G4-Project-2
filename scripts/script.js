9f0e9bbe5c32547408a7c6f46edb05c7f02b6ce1
Cloue Mac <maczyepez@gmail.com>
2025-11-14 14:44:54 -0800

    Add js script for the nav bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 /* Scripts */
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+let memuOpen = false;
+
+hamburger.addEventListener('click', () => {
+    if (!memuOpen) {
+        navLinks.style.display = 'block';
+        menuOpen = true;
+    }
+    else {
+        navLinks.style.display = 'none';
+        menuOpen = false;
+    }
+});
