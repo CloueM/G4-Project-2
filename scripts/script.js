802c9a1e62bff7d1f7142caf5e096e7df107afd8
Faith Ann <flumba@my.bcit.ca>
2025-12-03 19:49:54 -0800

    target all containers

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("accordion is working!")
+    console.log("accordion is working!");
+
+    const containers = document.querySelectorAll("#expect-section .container");
+    console.log("Containers are working!", containers.length);
+
 
     });
