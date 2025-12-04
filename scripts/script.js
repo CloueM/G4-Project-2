a77c43485bf4a31899b3118ac75dac1d1fe9c73c
Faith Ann <flumba@my.bcit.ca>
2025-12-03 19:57:24 -0800

    added functions for click to work so header can open body

--- scripts/script.js
+++ scripts/script.js
@@ -4,5 +4,17 @@ document.addEventListener("DOMContentLoaded", function() {
     const containers = document.querySelectorAll("#expect-section .container");
     console.log("Containers are working!", containers.length);
 
+    containers.forEach(function (container) {
+   const expectHeader = container.querySelector(".expect-header");
 
+   expectHeader.addEventListener("click", function() {
+
+    containers.forEach(function(otherContainer) {
+        otherContainer.classList.remove("is-open");   
     });
+        container.classList.add("is-open");
+        console.log("You clicked a container!", container);
+
+        });
+    });
+}); 
